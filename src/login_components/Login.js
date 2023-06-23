d8c6d7096b04bafba500e08dea9bfc0280dbad46
YuvarajSemalaiyappan <yuaaraj@gmail.com>
2023-06-23 16:56:33 +0530

    Updated

--- src/login_components/Login.js
+++ src/login_components/Login.js
@@ -49,7 +49,7 @@ export function Login() {
         <div className="row d-flex align-items-center justify-content-center h-100">
           <div className="col-md-8 col-lg-7 col-xl-6">
             <img
-              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+              src=""
               className="img-fluid login-img"
               alt="Phone image"
             />
--- src/login_components/Register.js
+++ src/login_components/Register.js
@@ -199,7 +199,7 @@ function Register() {
                   </div>
                   <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
                     <img
-                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                      src=""
                       class="img-fluid"
                       alt="Sample image"
                     />
